f62e061e049d06fd686cb5c7be51591a80ebc7e2
ozayo <ozay.ozdemir@gmail.com>
2024-10-17 09:54:42 +0200

    added Favorite movies page

---
+++ src/pages/FavoriteMovies.jsx
@@ -0,0 +1,39 @@
+// src/pages/FavoriteMovies.jsx
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import MovieList from '../components/MovieList';
+
+const FavoriteMovies = () => {
+  const favoriteIds = useSelector((state) => state.favorites.favorites);
+  const [favoriteMovies, setFavoriteMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchFavoriteMovies = async () => {
+      const promises = favoriteIds.map(async (id) => {
+        const response = await fetch(
+          `https://api.themoviedb.org/3/movie/${id}?api_key=${import.meta.env.VITE_API_KEY}`
+        );
+        return response.json();
+      });
+      const movies = await Promise.all(promises);
+      setFavoriteMovies(movies);
+    };
+
+    if (favoriteIds.length > 0) {
+      fetchFavoriteMovies();
+    }
+  }, [favoriteIds]);
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">My Favorite Movies</h1>
+      {favoriteMovies.length > 0 ? (
+        <MovieList movies={favoriteMovies} />
+      ) : (
+        <p>No favorite movies yet.</p>
+      )}
+    </div>
+  );
+};
+
+export default FavoriteMovies;
