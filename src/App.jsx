7bf67bc184d81124a2e41ff046becd89902ccce0
ozayo <ozay.ozdemir@gmail.com>
2024-10-17 15:06:27 +0200

    meta updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // src/App.jsx
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async'; // Helmet provider import
 import Home from './pages/Home';
 import Movies from './pages/Movies'; // Movies (index) varsayılan olarak popular filmleri gösterecek
 import TopRatedMovies from './pages/Movies/TopRatedMovies';
@@ -12,28 +13,44 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import MovieDetail from './pages/MovieDetail'; // MovieDetail bileşenini ekliyoruz
 import FavoriteMovies from './pages/FavoriteMovies'; // Favori filmler sayfasını ekliyoruz
+import ReactGA from 'react-ga4';
+import { useLocation } from 'react-router-dom';
+
+const Analytics = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.initialize('G-XLG80SZZRJ'); // GA4 Measurement ID
+    ReactGA.send({ hitType: 'pageview', page: location.pathname });
+  }, [location]);
+
+  return null;
+};
 
 const App = () => {
   return (
-    <Provider store={store}> {/* Redux store'u tüm uygulamaya sağlıyoruz */}
-      <Router>
-        <div>
-          <Header />
-          <main className="container mx-auto px-4 sm:px-6 lg:px-8 py-8 mt-8">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/movies" element={<Movies />} />  {/* Popular filmler */}
-              <Route path="/movies/top-rated" element={<TopRatedMovies />} />
-              <Route path="/movies/upcoming" element={<UpcomingMovies />} />
-              <Route path="/tvshows/*" element={<TVShows />} />
-              <Route path="/movies/:id" element={<MovieDetail />} /> {/* Detay sayfası */}
-              <Route path="/favorites" element={<FavoriteMovies />} /> {/* Favori Filmler sayfası */}
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
-    </Provider>
+    <HelmetProvider>
+      <Provider store={store}>
+        <Router>
+          <Analytics /> {/* Google Analytics'i izlemek için ekliyoruz */}
+          <div>
+            <Header />
+            <main className="container mx-auto px-4 sm:px-6 lg:px-8 py-8 mt-8">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/movies" element={<Movies />} />  
+                <Route path="/movies/top-rated" element={<TopRatedMovies />} />
+                <Route path="/movies/upcoming" element={<UpcomingMovies />} />
+                <Route path="/tvshows/*" element={<TVShows />} />
+                <Route path="/movies/:id" element={<MovieDetail />} />
+                <Route path="/favorites" element={<FavoriteMovies />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
+    </HelmetProvider>
   );
 };
 
