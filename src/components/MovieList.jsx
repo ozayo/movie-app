154405c21017f27e19aed95140c67efe8cfc48af
ozayo <ozay.ozdemir@gmail.com>
2024-10-17 12:23:52 +0200

    responsive fix

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,7 +4,7 @@ import MovieCard from './MovieCard';
 
 const MovieList = ({ movies }) => {
   return (
-    <div className="grid grid-cols-5 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
       {movies.map((movie) => (
         <MovieCard key={movie.id} movie={movie} />
       ))}
