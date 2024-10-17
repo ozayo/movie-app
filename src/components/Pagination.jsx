c767456c54159b6fc625b16c2c23f3f95f1e13a8
ozayo <ozay.ozdemir@gmail.com>
2024-10-17 12:22:36 +0200

    pagination fix

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 
 const Pagination = ({ currentPage, totalPages, onPageChange }) => {
-  const maxPagesToShow = 10; // Aynı anda gösterilecek maksimum sayfa sayısı
+  const maxPagesToShow = 5; // Aynı anda gösterilecek maksimum sayfa sayısı
   const startPage = Math.floor((currentPage - 1) / maxPagesToShow) * maxPagesToShow + 1;
   const endPage = Math.min(startPage + maxPagesToShow - 1, totalPages);
 
@@ -14,38 +14,75 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
 
   const handlePreviousGroup = () => {
     if (startPage > 1) {
-      onPageChange(startPage - 1);
+      onPageChange(startPage - maxPagesToShow);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      onPageChange(currentPage + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      onPageChange(currentPage - 1);
     }
   };
 
   const pages = Array.from({ length: endPage - startPage + 1 }, (_, index) => startPage + index);
 
   return (
-    <div className="flex justify-center mt-4">
+    <div className="flex justify-center mt-10 space-x-2">
+      {/* Önceki 5 sayfa << */}
       <button
-        className="px-4 py-2 mx-1 bg-gray-200"
+        className="px-2 py-1 bg-gray-200 hover:bg-gray-400 rounded-md"
         onClick={handlePreviousGroup}
         disabled={startPage === 1}
       >
-        Previous
+        &laquo;
       </button>
 
+      {/* Önceki sayfa < */}
+      <button
+        className="px-2 py-1 bg-gray-200 hover:bg-gray-400 rounded-md"
+        onClick={handlePreviousPage}
+        disabled={currentPage === 1}
+      >
+        &lt;
+      </button>
+
+      {/* Sayfa numaraları */}
       {pages.map((page) => (
         <button
           key={page}
-          className={`px-4 py-2 mx-1 ${page === currentPage ? 'bg-gray-800 text-white' : 'bg-gray-200 hover:bg-gray-800 hover:text-white'}`}
+          className={`px-2 py-1 rounded-md ${
+            page === currentPage
+              ? 'bg-gray-800 text-white'
+              : 'bg-gray-200 hover:bg-gray-800 hover:text-white'
+          }`}
           onClick={() => onPageChange(page)}
         >
           {page}
         </button>
       ))}
 
+      {/* Sonraki sayfa > */}
       <button
-        className="px-4 py-2 mx-1 bg-gray-200"
+        className="px-2 py-1 bg-gray-200 hover:bg-gray-400 rounded-md"
+        onClick={handleNextPage}
+        disabled={currentPage === totalPages}
+      >
+        &gt;
+      </button>
+
+      {/* Sonraki 5 sayfa >> */}
+      <button
+        className="px-2 py-1 bg-gray-200 hover:bg-gray-400 rounded-md"
         onClick={handleNextGroup}
         disabled={endPage === totalPages}
       >
-        Next
+        &raquo;
       </button>
     </div>
   );
