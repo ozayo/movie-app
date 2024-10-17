41437d7f8406e1ec7e23ecb4b2417a47a8743116
ozayo <ozay.ozdemir@gmail.com>
2024-10-17 09:38:32 +0200

    added add favori heart icon & function

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,25 @@
 // src/components/MovieCard.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaStar } from "react-icons/fa";
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleFavorite } from '../redux/slices/favoritesSlice';
+import { FaHeart, FaStar } from 'react-icons/fa';
 
 const MovieCard = ({ movie }) => {
+  const dispatch = useDispatch();
+  const favorites = useSelector((state) => state.favorites.favorites);
+
+  // Bu film favori listesinde mi?
+  const isFavorite = favorites.includes(movie.id);
+
+  // Favorilere ekleme/çıkarma işlemi
+  const handleFavoriteToggle = () => {
+    dispatch(toggleFavorite(movie));
+  };
+
   return (
-    
-    <div className="bg-gray-800 p-4 rounded-lg shadow hover:bg-gray-900 hover:shadow-lg">
-      <Link to={`/movies/${movie.id}`} className="block">
+    <div className="bg-gray-800 p-4 rounded-lg shadow hover:bg-gray-900 hover:shadow-lg relative">
+      <Link to={`/movies/${movie.id}`}>
         <img
           src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
           alt={movie.title}
@@ -21,11 +33,20 @@ const MovieCard = ({ movie }) => {
             <p className="text-gray-400"> {movie.vote_average.toFixed(1)}</p>
           </div>
         </div>
-        </Link>
-      </div>
-    
+      </Link>
+
+      {/* Kalp İkonu */}
+      <FaHeart
+        onClick={handleFavoriteToggle}
+        className={`absolute top-2 right-2 cursor-pointer ${
+          isFavorite ? 'text-red-500' : 'text-white'
+        }`}
+        size={24}
+      />
+    </div>
   );
 };
 
 export default MovieCard;
+
 
